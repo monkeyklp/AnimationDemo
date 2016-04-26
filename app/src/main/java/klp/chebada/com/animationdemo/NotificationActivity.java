a110cbbc8ed74e06b329047a349fb7c47f93900c
monkey <klp13115@ly.com>
2016-04-26 13:59:46 +0800

    add notification

--- app/src/main/java/klp/chebada/com/animationdemo/NotificationActivity.java
+++ app/src/main/java/klp/chebada/com/animationdemo/NotificationActivity.java
@@ -1,9 +1,11 @@
 package klp.chebada.com.animationdemo;
 
 import android.app.Activity;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
@@ -36,26 +38,29 @@ public class NotificationActivity extends AppCompatActivity {
     }
 
     public void buildNotification(){
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
+        NotificationCompat.BigTextStyle bigStyle = new NotificationCompat.BigTextStyle();
+        bigStyle.bigText("bigStyle");
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
                         .setSmallIcon(R.mipmap.notification_icon)
                         .setContentTitle("My notification")
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
 //                        .setColor()
-                        .setContentText("Hello World!");
-        mBuilder.setContentIntent(deFaultResultIntent());
+                        .setDefaults(Notification.DEFAULT_ALL)
+                        .setContentText("Hello World!")
+                        .setStyle(bigStyle)
+                        .setAutoCancel(true)
+                        .setContentIntent(deFaultResultIntent());
 
         int mNotificationId = 001;
 // Gets an instance of the NotificationManager service
-        NotificationManager mNotifyMgr =
-                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        NotificationManager mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 // Builds the notification and issues it.
         mNotifyMgr.notify(mNotificationId, mBuilder.build());
     }
 
     private PendingIntent deFaultResultIntent(){
         Intent intent = new Intent(this, NotificationActivity.class);
-        PendingIntent resultPendingIntent =
-                PendingIntent.getActivity(
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(
                         this,
                         REQUEST_CODE,
                         intent,
