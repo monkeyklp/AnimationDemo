ddcc8455ed6cf3b00389640c1ebb2d4877cbd30f
monkey <klp13115@ly.com>
2017-01-16 16:43:17 +0800

    fix animation bug

--- app/src/main/java/klp/chebada/com/animationdemo/behavior/FooterBehavior.java
+++ app/src/main/java/klp/chebada/com/animationdemo/behavior/FooterBehavior.java
@@ -1,12 +1,12 @@
 package klp.chebada.com.animationdemo.behavior;
 
-import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewPropertyAnimator;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
 
@@ -15,98 +15,72 @@ import android.view.animation.LinearInterpolator;
  */
 public class FooterBehavior extends CoordinatorLayout.Behavior {
 
+    private static final String TAG = "FooterBehavior";
+
     private static final Interpolator INTERPOLATOR = new LinearInterpolator();
-
-
-    private int sinceDirectionChange;
-
 
     public FooterBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
+    private ValueAnimator valueAnimator;
+
     //1.判断滑动的方向 我们需要垂直滑动
     @Override
     public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, View child, View directTargetChild, View target, int nestedScrollAxes) {
-        return (nestedScrollAxes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0;
+        final boolean started = (nestedScrollAxes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0
+                //内容超过一屏，可滚动
+                && coordinatorLayout.getHeight() - directTargetChild.getHeight() <= child.getHeight();
+        if(started && valueAnimator != null) {
+            valueAnimator.cancel();
+        }
+        return started;
     }
 
     //2.根据滑动的距离显示和隐藏footer view
     @Override
     public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dx, int dy, int[] consumed) {
-        if (dy > 0 && sinceDirectionChange < 0 || dy < 0 && sinceDirectionChange > 0) {
-            child.animate().cancel();
-            sinceDirectionChange = 0;
-        }
-        sinceDirectionChange += dy;
-        if (sinceDirectionChange > child.getHeight() && child.getVisibility() == View.VISIBLE) {
-            hide(child);
-        } else if (sinceDirectionChange < 0 && child.getVisibility() == View.GONE) {
-            show(child);
-        }
+        Log.e(TAG, "onNestedPreScroll--------dy " + dy);
+        Log.e(TAG, "onNestedPreScroll--------consumed " + consumed[1]);
     }
 
     @Override
     public void onNestedScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
-        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+        Log.e(TAG, "onNestedScroll--------dyConsumed " + dyConsumed);
+        Log.e(TAG, "onNestedScroll--------dyUnconsumed " + dyUnconsumed);
+        Log.e(TAG, "onNestedScroll--------target " + target.getClass().getSimpleName());
+        Log.e(TAG, "onNestedScroll--------child " + child.getClass().getSimpleName());
+        if (dyConsumed > 0 ) {
             // User scrolled down and the FAB is currently visible -> hide the FAB
-            hide(child);
-        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+//            hide(child);
+            animator(child, false);
+        } else if (dyConsumed < 0) {
             // User scrolled up and the FAB is currently not visible -> show the FAB
-           show(child);
+//            show(child);
+            animator(child, true);
+     }
+    }
+    private void animator(final View view, boolean up) {
+
+        if(valueAnimator == null) {
+            valueAnimator = new ValueAnimator();
+            valueAnimator.setDuration(200);
+            valueAnimator.setInterpolator(INTERPOLATOR);
+            valueAnimator.setTarget(view);
+            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                    view.setTranslationY((Float)valueAnimator.getAnimatedValue());
+                }
+            });
+        } else {
+            valueAnimator.cancel();
         }
-    }
-
-    private void hide(final View view) {
-        ViewPropertyAnimator animator = view.animate().translationY(view.getHeight()).setInterpolator(INTERPOLATOR).setDuration(200);
-        animator.setListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animator) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animator) {
-                view.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animator) {
-                show(view);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animator) {
-
-            }
-        });
-        animator.start();
-    }
-
-
-    private void show(final View view) {
-        ViewPropertyAnimator animator = view.animate().translationY(0).setInterpolator(INTERPOLATOR).setDuration(200);
-        animator.setListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animator) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animator) {
-                view.setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animator) {
-                hide(view);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animator) {
-
-            }
-        });
-        animator.start();
+        if(up) {
+            valueAnimator.setFloatValues(view.getTranslationY(), 0);
+        } else {
+            valueAnimator.setFloatValues(view.getTranslationY(), view.getHeight());
+        }
+        valueAnimator.start();
     }
 }
