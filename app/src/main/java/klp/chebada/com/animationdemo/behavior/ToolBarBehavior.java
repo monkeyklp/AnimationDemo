a68153c11852e022ffa0983806772f94f5880121
monkey <klp13115@ly.com>
2017-03-14 21:48:05 +0800

    fix bug

--- app/src/main/java/klp/chebada/com/animationdemo/behavior/ToolBarBehavior.java
+++ app/src/main/java/klp/chebada/com/animationdemo/behavior/ToolBarBehavior.java
@@ -2,19 +2,18 @@ package klp.chebada.com.animationdemo.behavior;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
-import android.support.v4.view.ViewCompat;
 import android.support.v4.view.animation.FastOutLinearInInterpolator;
 import android.support.v4.view.animation.LinearOutSlowInInterpolator;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.Interpolator;
 
 /**
  * Created by klp13115 on 2016/4/11.
  */
-public class ToolBarBehavior extends CoordinatorLayout.Behavior {
+public class ToolBarBehavior extends AppBarLayout.Behavior {
 
     private static final String TAG = "ToolBarBehavior";
 
@@ -28,41 +27,40 @@ public class ToolBarBehavior extends CoordinatorLayout.Behavior {
 
     private ValueAnimator valueAnimator;
 
-    //1.判断滑动的方向 我们需要垂直滑动
-    @Override
-    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, View child, View directTargetChild, View target, int nestedScrollAxes) {
-        final boolean started = (nestedScrollAxes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0
-                //内容超过一屏，可滚动
-                && coordinatorLayout.getHeight() - directTargetChild.getHeight() <= child.getHeight();
-        if(started && valueAnimator != null) {
-            valueAnimator.cancel();
-        }
-        return started;
-    }
+//    //1.判断滑动的方向 我们需要垂直滑动
+//    @Override
+//    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, View child, View directTargetChild, View target, int nestedScrollAxes) {
+//        final boolean started = (nestedScrollAxes & ViewCompat.SCROLL_AXIS_VERTICAL) != 0
+//                //内容超过一屏，可滚动
+//                && coordinatorLayout.getHeight() - directTargetChild.getHeight() <= child.getHeight();
+//        if(started && valueAnimator != null && valueAnimator.isRunning()) {
+//            valueAnimator.end();
+//            Log.e(TAG, "Animator cancel");
+//        }
+//        return started;
+//    }
 
-    //2.根据滑动的距离显示和隐藏footer view
-    @Override
-    public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dx, int dy, int[] consumed) {
-        Log.e(TAG, "onNestedPreScroll--------dy " + dy);
-        Log.e(TAG, "onNestedPreScroll--------consumed " + consumed[1]);
-    }
 
     @Override
-    public void onNestedScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
-        Log.e(TAG, "onNestedScroll--------dyConsumed " + dyConsumed);
-        Log.e(TAG, "onNestedScroll--------dyUnconsumed " + dyUnconsumed);
-        Log.e(TAG, "onNestedScroll--------target " + target.getClass().getSimpleName());
-        Log.e(TAG, "onNestedScroll--------child " + child.getClass().getSimpleName());
-        if (dyConsumed > 0 ) {
-            // User scrolled down and the FAB is currently visible -> hide the FAB
-//            hide(child);
-            animator(child, false);
-        } else if (dyConsumed < 0) {
-            // User scrolled up and the FAB is currently not visible -> show the FAB
-//            show(child);
-            animator(child, true);
-     }
+    public boolean onStartNestedScroll(CoordinatorLayout parent, AppBarLayout child, View directTargetChild, View target, int nestedScrollAxes) {
+        return super.onStartNestedScroll(parent, child, directTargetChild, target, nestedScrollAxes);
     }
+
+    //    @Override
+//    public void onNestedScroll(CoordinatorLayout coordinatorLayout, View child, View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+//        Log.e(TAG, "onNestedScroll--------dyConsumed " + dyConsumed);
+//        Log.e(TAG, "onNestedScroll--------dyUnconsumed " + dyUnconsumed);
+//        if (dyConsumed > 2) {
+//            // User scrolled down and the FAB is currently visible -> hide the FAB
+////            hide(child);
+//            animator(child, false);
+//
+//        } else if (dyConsumed < -2) {
+//            // User scrolled up and the FAB is currently not visible -> show the FAB
+////            show(child);
+//            animator(child, true);
+//     }
+//    }
     private void animator(final View view, boolean up) {
 
         if(valueAnimator == null) {
@@ -75,8 +73,6 @@ public class ToolBarBehavior extends CoordinatorLayout.Behavior {
                     view.setTranslationY((Float)valueAnimator.getAnimatedValue());
                 }
             });
-        } else {
-            valueAnimator.cancel();
         }
         if(up) {
             valueAnimator.setInterpolator(ACCELERATION_INTERPOLATOR);
