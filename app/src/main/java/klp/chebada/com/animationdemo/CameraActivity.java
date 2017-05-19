39b00bd28efe10b54441b266749eb91785fb3980
monkey <klp13115@ly.com>
2017-05-19 18:03:44 +0800

    add

--- app/src/main/java/klp/chebada/com/animationdemo/CameraActivity.java
+++ app/src/main/java/klp/chebada/com/animationdemo/CameraActivity.java
@@ -5,32 +5,43 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.SurfaceHolder;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
 
 import java.util.Arrays;
 
 import klp.chebada.com.animationdemo.databinding.ActivityCameraBinding;
 
-public class CameraActivity extends AppCompatActivity implements SurfaceHolder.Callback{
+public class CameraActivity extends AppCompatActivity  implements TextureView.SurfaceTextureListener{
 
     private ActivityCameraBinding mBinding;
 
     private CameraManager mCameraManager;
 
+    private Size mPreviewSize;
+
     private String mCameraId = ""+CameraCharacteristics.LENS_FACING_FRONT;
 
     private CameraDevice mCameraDevice;
+
+    private Handler mHandler;
 
     public static void startActivity(Activity fromActivity){
         Intent intent = new Intent(fromActivity, CameraActivity.class);
@@ -41,32 +52,10 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding  = DataBindingUtil.setContentView(this, R.layout.activity_camera);
-        mBinding.surfaceView.getHolder().addCallback(this);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    @Override
-    public void surfaceCreated(SurfaceHolder surfaceHolder) {
-        mCameraManager = (CameraManager)getSystemService(CAMERA_SERVICE);
-        try {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
-            mCameraManager.openCamera(mCameraId, stateCallback, null);
-        }catch (Exception e) {
-
-        }
-
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-
+        HandlerThread handlerThread = new HandlerThread("camera2");
+        handlerThread.start();
+        mHandler = new Handler(handlerThread.getLooper());
+        mBinding.surfaceView.setSurfaceTextureListener(this);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -93,21 +82,22 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
     private void takePreview(){
         try {
             final CaptureRequest.Builder builder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            builder.addTarget(mBinding.surfaceView.getHolder().getSurface());
-            mCameraDevice.createCaptureSession(Arrays.asList(mBinding.surfaceView.getHolder().getSurface()),
+            SurfaceTexture surfaceTexture =  mBinding.surfaceView.getSurfaceTexture();
+            surfaceTexture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+            Surface surface = new Surface(surfaceTexture);
+            builder.addTarget(surface);
+            builder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
+            //打开闪光灯
+            builder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
+            // 显示预览
+            final CaptureRequest previewRequest = builder.build();
+            mCameraDevice.createCaptureSession(Arrays.asList(surface),
                     new CameraCaptureSession.StateCallback() {
                         @Override
                         public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
                             try {
-                                // 自动对焦
-                                builder.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
-                                //打开闪光灯
-                                builder.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
-                                // 显示预览
-                                CaptureRequest previewRequest = builder.build();
-                                cameraCaptureSession.setRepeatingRequest(previewRequest, null, null );
+                                cameraCaptureSession.setRepeatingRequest(previewRequest, null, mHandler );
                             }catch (Exception e) {
-
                             }
                         }
 
@@ -117,7 +107,43 @@ public class CameraActivity extends AppCompatActivity implements SurfaceHolder.C
                         }
                     }, null);
         } catch (Exception e) {
-
         }
+    }
+
+    @RequiresApi(api = 21)
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        try {
+            //获得CameraManager
+            mCameraManager = (CameraManager)getSystemService(CAMERA_SERVICE);
+            //获得属性
+            CameraCharacteristics characteristics = mCameraManager.getCameraCharacteristics(""+CameraCharacteristics.LENS_FACING_FRONT);
+            //支持的STREAM CONFIGURATION
+            StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            //显示的size
+            mPreviewSize = map.getOutputSizes(SurfaceTexture.class)[0];
+            //打开相机
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mCameraManager.openCamera("0", stateCallback, mHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
     }
 }
