be4d454f6b9432cb3a9eba538e9ce11dfcec54d4
monkey <klp13115@ly.com>
2020-11-04 23:41:15 +0800

    SVGA

--- app/src/main/java/klp/com/animationdemo/BesselActivity.kt
+++ app/src/main/java/klp/com/animationdemo/BesselActivity.kt
@@ -3,8 +3,12 @@ package klp.com.animationdemo
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import klp.com.animationdemo.view.MoveBall3
+import com.opensource.svgaplayer.SVGADrawable
+import com.opensource.svgaplayer.SVGAImageView
+import com.opensource.svgaplayer.SVGAParser
+import com.opensource.svgaplayer.SVGAVideoEntity
 
 class BesselActivity : AppCompatActivity() {
 
@@ -18,6 +22,20 @@ class BesselActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bessel)
+        SVGAParser(this).parse("Goddess.svga", object: SVGAParser.ParseCompletion{
+            override fun onComplete(videoItem: SVGAVideoEntity) {
+                findViewById<SVGAImageView>(R.id.svg).setImageDrawable(SVGADrawable(videoItem))
+                findViewById<SVGAImageView>(R.id.svg).startAnimation()
+            }
+
+            override fun onError() {
+            }
+
+        })
+        findViewById<Button>(R.id.btn).setOnClickListener {
+            findViewById<SVGAImageView>(R.id.svg).stopAnimation()
+           findViewById<SVGAImageView>(R.id.svg).visibility = View.GONE
+        }
 //        val moveView = findViewById<MoveBall3>(R.id.moveBall)
 //        moveView.setCallback { x, y -> moveView.scrollTo(x, y) }
     }
