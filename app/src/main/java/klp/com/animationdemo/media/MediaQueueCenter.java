4f51051c98253769b08436fa29d09d1bc48bc8ba
monkey <klp13115@ly.com>
2022-05-18 17:01:49 +0800

    center

--- app/src/main/java/klp/com/animationdemo/media/MediaQueueCenter.java
+++ app/src/main/java/klp/com/animationdemo/media/MediaQueueCenter.java
@@ -15,12 +15,22 @@ public class MediaQueueCenter {
     private HandlerThread mHandlerThread = new HandlerThread("MediaLooper");
     private Handler mWorkHandler;
     private int MSG_EXPENSE_TASK = 1;
+    private QueueItem mCurrentQueueItem;
+    private Boolean isPlayRunning = false;
+
+    private MediaQueueCenter() {}
+    private static class SingletonInstance {
+        private static final MediaQueueCenter instance = new MediaQueueCenter();
+    }
+    public static MediaQueueCenter getInstance() {
+        return SingletonInstance.instance;
+    }
 
     public void enqueue(String song) {
-        try {
-            mQueue.put(song);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        mQueue.offer(song);
+        if (mWorkHandler != null) {
+            mWorkHandler.removeMessages(MSG_EXPENSE_TASK);
+            mWorkHandler.sendEmptyMessage(MSG_EXPENSE_TASK);
         }
     }
 
@@ -31,10 +41,12 @@ public class MediaQueueCenter {
             @Override
             public void handleMessage(Message msg) {
                 if (msg.what == MSG_EXPENSE_TASK) {
-                    if (mQueue.isEmpty()) return;
+                    if (mQueue.isEmpty() || isPlayRunning) return;
                     QueueItem item = null;
                     try {
                         item = (QueueItem) mQueue.take();
+                        mCurrentQueueItem = item;
+                        isPlayRunning = true;
                         item.run(new Function() {
                             @Override
                             public Object apply(Object o) {
@@ -42,22 +54,28 @@ public class MediaQueueCenter {
                                 return null;
                             }
                         });
+                        mCurrentQueueItem = null;
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }
             }
         };
-
     }
 
     private void queryNext() {
+        isPlayRunning = false;
         mWorkHandler.sendEmptyMessage(MSG_EXPENSE_TASK);
     }
 
+    public void stop() {
+
+    }
+
     public void quit() {
+        isPlayRunning = false;
         mQueue.clear();
-        mWorkHandler.removeMessages(MSG_EXPENSE_TASK);
+        mWorkHandler.removeCallbacksAndMessages(null);
         mHandlerThread.quit();
     }
 
